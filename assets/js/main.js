94218d1b9360ca6718c5421ecb5f746033fb91d1
JoeGitHubPro <95888292+JoeGitHubPro@users.noreply.github.com>
2023-05-31 01:52:25 +0300

    Edit-summer

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,98 +44,98 @@
     })
   }
 
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  // /**
+  //  * Mobile nav toggle
+  //  */
+  // on('click', '.mobile-nav-toggle', function(e) {
+  //   select('#navbar').classList.toggle('navbar-mobile')
+  //   this.classList.toggle('bi-list')
+  //   this.classList.toggle('bi-x')
+  // })
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '#navbar .nav-link', function(e) {
-    let section = select(this.hash)
-    if (section) {
-      e.preventDefault()
+  // /**
+  //  * Scrool with ofset on links with a class name .scrollto
+  //  */
+  // on('click', '#navbar .nav-link', function(e) {
+  //   let section = select(this.hash)
+  //   if (section) {
+  //     e.preventDefault()
 
-      let navbar = select('#navbar')
-      let header = select('#header')
-      let sections = select('section', true)
-      let navlinks = select('#navbar .nav-link', true)
+  //     let navbar = select('#navbar')
+  //     let header = select('#header')
+  //     let sections = select('section', true)
+  //     let navlinks = select('#navbar .nav-link', true)
 
-      navlinks.forEach((item) => {
-        item.classList.remove('active')
-      })
+  //     navlinks.forEach((item) => {
+  //       item.classList.remove('active')
+  //     })
 
-      this.classList.add('active')
+  //     this.classList.add('active')
 
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
+  //     if (navbar.classList.contains('navbar-mobile')) {
+  //       navbar.classList.remove('navbar-mobile')
+  //       let navbarToggle = select('.mobile-nav-toggle')
+  //       navbarToggle.classList.toggle('bi-list')
+  //       navbarToggle.classList.toggle('bi-x')
+  //     }
 
-      if (this.hash == '#header') {
-         header.classList.remove('header-top')
-        sections.forEach((item) => {
-          item.classList.remove('section-show')
-        })
-        return;
-      }
+  //     if (this.hash == '#header') {
+  //        header.classList.remove('header-top')
+  //       sections.forEach((item) => {
+  //         item.classList.remove('section-show')
+  //       })
+  //       return;
+  //     }
 
-      if (!header.classList.contains('header-top')) {
-        header.classList.add('header-top')
-        setTimeout(function() {
-          sections.forEach((item) => {
-            item.classList.remove('section-show')
-          })
-          section.classList.add('section-show')
+  //     if (!header.classList.contains('header-top')) {
+  //       header.classList.add('header-top')
+  //       setTimeout(function() {
+  //         sections.forEach((item) => {
+  //           item.classList.remove('section-show')
+  //         })
+  //         section.classList.add('section-show')
 
-        }, 350);
-      } else {
-        sections.forEach((item) => {
-          item.classList.remove('section-show')
-        })
-        section.classList.add('section-show')
-      }
+  //       }, 350);
+  //     } else {
+  //       sections.forEach((item) => {
+  //         item.classList.remove('section-show')
+  //       })
+  //       section.classList.add('section-show')
+  //     }
 
-      scrollto(this.hash)
-    }
-  }, true)
+  //     scrollto(this.hash)
+  //   }
+  // }, true)
 
-  /**
-   * Activate/show sections on load with hash links
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      let initial_nav = select(window.location.hash)
+  // /**
+  //  * Activate/show sections on load with hash links
+  //  */
+  // window.addEventListener('load', () => {
+  //   if (window.location.hash) {
+  //     let initial_nav = select(window.location.hash)
 
-      if (initial_nav) {
-        let header = select('#header')
-        let navlinks = select('#navbar .nav-link', true)
+  //     if (initial_nav) {
+  //       let header = select('#header')
+  //       let navlinks = select('#navbar .nav-link', true)
 
-        header.classList.add('header-top')
+  //       header.classList.add('header-top')
 
-        navlinks.forEach((item) => {
-          if (item.getAttribute('href') == window.location.hash) {
-            item.classList.add('active')
-          } else {
-            item.classList.remove('active')
-          }
-        })
+  //       navlinks.forEach((item) => {
+  //         if (item.getAttribute('href') == window.location.hash) {
+  //           item.classList.add('active')
+  //         } else {
+  //           item.classList.remove('active')
+  //         }
+  //       })
 
-        setTimeout(function() {
-          initial_nav.classList.add('section-show')
-        }, 350);
+  //       setTimeout(function() {
+  //         initial_nav.classList.add('section-show')
+  //       }, 350);
 
-        scrollto(window.location.hash)
-      }
-    }
-  });
+  //       scrollto(window.location.hash)
+  //     }
+  //   }
+  // });
 
   /**
    * Skills animation
