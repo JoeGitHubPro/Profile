d0549ec38324344f4a144d8f2079d554e9c4c71e
Youssef Mohamed Ali <95888292+JoeGitHubPro@users.noreply.github.com>
2026-06-14 21:07:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/youssef-mohamed-ali-portfolio/', // 👈 Add this line
+    base: '/Profile/', // 👈 Add this line
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
